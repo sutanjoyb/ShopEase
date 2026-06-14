0cd7db872ff927e67c3462c4868d557fea9ee65b
SutanjoyB <bsutanjoy@gmail.com>
2026-06-14 13:48:05 +0530

    Updated API

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,17 @@
-function ProductCard({ title, price, onAddToCart }) {
+function ProductCard({ title, price, image }) {
   return (
-    <div className="bg-white rounded-xl shadow-md p-5 hover:-translate-y-3 hover:shadow-2xl transition-all duration-300 cursor-pointer">
-      <div className="h-48 bg-gray-200 rounded-lg mb-4"></div>
+    <div className="bg-white rounded-xl shadow-md p-4 hover:-translate-y-2 hover:shadow-2xl transition-all duration-300">
+      <img
+        src={image}
+        alt={title}
+        className="h-48 w-full object-cover rounded-lg mb-4"
+      />
 
-      <h3 className="text-center text-lg font-semibold mb-2">{title}</h3>
+      <h3 className="text-lg font-semibold mb-2">{title}</h3>
 
-      <p className="text-center text-2xl font-bold text-blue-600 mb-4">
-        ₹{price}
-      </p>
+      <p className="text-blue-600 text-2xl font-bold mb-4">${price}</p>
 
-      <button
-        onClick={onAddToCart}
-        className="cursor-pointer w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 hover:scale-105 transition-all duration-300"
-      >
+      <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">
         Add to Cart
       </button>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import ProductCard from "../components/ProductCard";
 import Footer from "../components/Footer";
 
 function Home() {
+  const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(true);
 
-  const products = [
-    {
-      id: 1,
-      title: "Wireless Headphones",
-      price: 2499,
-    },
-    {
-      id: 2,
-      title: "Gaming Mouse",
-      price: 1499,
-    },
-    {
-      id: 3,
-      title: "Mechanical Keyboard",
-      price: 3999,
-    },
-    {
-      id: 4,
-      title: "Epson Printer",
-      price: 8999,
-    },
-    {
-      id: 5,
-      title: "USB Connector",
-      price: 499,
-    },
-    {
-      id: 6,
-      title: "Smart Watch",
-      price: 1999,
-    },
-  ];
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/products");
+
+      const data = await response.json();
+
+      setProducts(data.products);
+    } catch (error) {
+      console.log("Error fetching products:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   const filteredProducts = products.filter((product) =>
     product.title.toLowerCase().includes(searchTerm.toLowerCase()),
   );
 
-  const handleAddToCart = (product) => {
-    console.log("Added:", product.title);
-  };
+  if (loading) {
+    return (
+      <div className="min-h-screen flex justify-center items-center text-3xl font-bold">
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <>
@@ -62,20 +53,20 @@ function Home() {
         <div className="max-w-md mx-auto mb-10">
           <input
             type="text"
-            placeholder="Search products..."
+            placeholder="Search Products..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             className="w-full border border-gray-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {filteredProducts.map((product) => (
             <ProductCard
               key={product.id}
               title={product.title}
               price={product.price}
-              onAddToCart={() => handleAddToCart(product)}
+              image={product.thumbnail}
             />
           ))}
         </div>
